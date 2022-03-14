f788d4f2d287cad9c20abbba6e49c6e67ec0147b
Char2s <Char2s@outlook.com>
2022-03-14 21:38:31 +0800

    feat(server/app): apply `ValidationPipe` globally

--- pennfoster-helper.server/apps/app/src/app.module.ts
+++ pennfoster-helper.server/apps/app/src/app.module.ts
@@ -1,9 +1,11 @@
-import { Module } from '@nestjs/common';
+import { Module, ValidationPipe } from '@nestjs/common';
+import { APP_PIPE } from '@nestjs/core';
 
 import { AnswersModule } from './answers/answers.module';
 import { CoreModule } from './core/core.module';
 
 @Module({
   imports: [CoreModule, AnswersModule],
+  providers: [{ provide: APP_PIPE, useClass: ValidationPipe }],
 })
 export class AppModule {}
