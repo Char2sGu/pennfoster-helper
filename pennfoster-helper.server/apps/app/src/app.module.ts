500ac7a58f69ca2a01f034d027e4656a92c2b6a7
Char2s <Char2s@outlook.com>
2022-03-14 21:26:24 +0800

    feat(server/app): remove example code

--- pennfoster-helper.server/apps/app/src/app.module.ts
+++ pennfoster-helper.server/apps/app/src/app.module.ts
@@ -1,12 +1,8 @@
 import { Module } from '@nestjs/common';
 
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { CoreModule } from './core/core.module';
 
 @Module({
   imports: [CoreModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
