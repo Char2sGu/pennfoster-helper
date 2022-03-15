2b9eff42978c39bbcef419a05404d6b48841d7b3
Char2s <Char2s@outlook.com>
2022-03-15 10:11:51 +0800

    feat(client/ext/core): init

--- pennfoster-helper.client/projects/browser-extension/src/app/app.module.ts
+++ pennfoster-helper.client/projects/browser-extension/src/app/app.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, CoreModule],
   providers: [],
   bootstrap: [AppComponent],
 })
