f48fd71161673c564388d737b1bbe6ebc138b042
Char2s <Char2s@outlook.com>
2022-03-15 21:24:15 +0800

    feat(client/ext/app): handle loading state

--- pennfoster-helper.client/projects/browser-extension/src/app/app.component.ts
+++ pennfoster-helper.client/projects/browser-extension/src/app/app.component.ts
@@ -1,4 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Overlay, OverlayRef } from '@angular/cdk/overlay';
+import { TemplatePortal } from '@angular/cdk/portal';
+import {
+  AfterViewInit,
+  Component,
+  OnInit,
+  TemplateRef,
+  ViewChild,
+  ViewContainerRef,
+} from '@angular/core';
 
 import { Bridge } from './core/bridge.service';
 import { Weegy, WeegyDialog } from './core/weegy.service';
@@ -8,18 +17,54 @@ import { Weegy, WeegyDialog } from './core/weegy.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, AfterViewInit {
   selectedIndex = 0;
   dialogs: WeegyDialog[] = [];
   dialogCurrentIndex = 0;
+
+  @ViewChild('spinner')
+  private spinnerTemplateRef!: TemplateRef<unknown>;
+  private spinnerTemplatePortal!: TemplatePortal<unknown>;
+  private spinnerOverlayRef!: OverlayRef;
+
+  private _loading = true;
+  public get loading(): boolean {
+    return this._loading;
+  }
+  public set loading(v: boolean) {
+    this._loading = v;
+    if (this.loading) this.spinnerOverlayRef.attach(this.spinnerTemplatePortal);
+    else this.spinnerOverlayRef.detach();
+  }
 
   get dialogCurrent(): WeegyDialog {
     return this.dialogs[this.dialogCurrentIndex];
   }
 
-  constructor(private bridge: Bridge, private weegy: Weegy) {}
+  constructor(
+    private viewContainerRef: ViewContainerRef,
+    private overlay: Overlay,
+    private bridge: Bridge,
+    private weegy: Weegy,
+  ) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  ngAfterViewInit(): void {
+    this.spinnerOverlayRef = this.overlay.create({
+      positionStrategy: this.overlay
+        .position()
+        .global()
+        .centerHorizontally()
+        .centerVertically(),
+      hasBackdrop: true,
+    });
+    this.spinnerTemplatePortal = new TemplatePortal(
+      this.spinnerTemplateRef,
+      this.viewContainerRef,
+    );
+
+    this.loading = true;
     this.bridge
       .execute(
         () =>
@@ -27,10 +72,11 @@ export class AppComponent implements OnInit {
             ?.innerText,
       )
       .subscribe((question) => {
-        if (question)
-          this.weegy
-            .search(question)
-            .subscribe((dialogs) => (this.dialogs = dialogs));
+        if (!question) return; // TODO
+        this.weegy.search(question).subscribe((dialogs) => {
+          this.dialogs = dialogs;
+          this.loading = false;
+        });
       });
   }
 }
--- pennfoster-helper.client/projects/browser-extension/src/app/app.module.ts
+++ pennfoster-helper.client/projects/browser-extension/src/app/app.module.ts
@@ -1,7 +1,10 @@
+import { OverlayModule } from '@angular/cdk/overlay';
+import { PortalModule } from '@angular/cdk/portal';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
@@ -21,6 +24,9 @@ import { CoreModule } from './core/core.module';
     MatIconModule,
     MatToolbarModule,
     MatSidenavModule,
+    MatProgressSpinnerModule,
+    OverlayModule,
+    PortalModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
