c88139b9254edeb9d7aacd23a7d15775342b9758
Char2s <Char2s@outlook.com>
2022-03-15 09:59:50 +0800

    feat(client/ext/app): remove example code

--- pennfoster-helper.client/projects/browser-extension/src/app/app.component.spec.ts
+++ pennfoster-helper.client/projects/browser-extension/src/app/app.component.spec.ts
@@ -14,19 +14,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
-
-  it(`should have as title 'browser-extension'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('browser-extension');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain(
-      'browser-extension app is running!',
-    );
-  });
 });
