2edbed2cea894375c61eecd12d5997a9ce8d5c5e
Char2s <Char2s@outlook.com>
2022-03-15 10:54:01 +0800

    feat(client/ext): add Angular Material

--- pennfoster-helper.client/projects/browser-extension/src/app/app.module.ts
+++ pennfoster-helper.client/projects/browser-extension/src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, CoreModule],
+  imports: [BrowserModule, BrowserAnimationsModule, CoreModule],
   providers: [],
   bootstrap: [AppComponent],
 })
