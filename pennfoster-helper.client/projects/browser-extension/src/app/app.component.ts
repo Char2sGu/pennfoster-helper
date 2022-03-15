3546564c413ce79186769f5f8bdf1aceb144f19e
Char2s <Char2s@outlook.com>
2022-03-15 19:11:28 +0800

    feat(client/ext/app): implement basic question search

--- pennfoster-helper.client/projects/browser-extension/src/app/app.component.ts
+++ pennfoster-helper.client/projects/browser-extension/src/app/app.component.ts
@@ -1,8 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { Bridge } from './core/bridge.service';
+import { Weegy, WeegyDialog } from './core/weegy.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  dialogs: WeegyDialog[] = [];
+
+  constructor(private bridge: Bridge, private weegy: Weegy) {}
+
+  ngOnInit(): void {
+    this.bridge
+      .execute(
+        () =>
+          document.querySelector<HTMLSpanElement>('span.bordered-content')
+            ?.innerText,
+      )
+      .subscribe((question) => {
+        if (question)
+          this.weegy
+            .search(question)
+            .subscribe((dialogs) => (this.dialogs = dialogs));
+      });
+  }
+}
